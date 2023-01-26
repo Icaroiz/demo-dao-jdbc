accfb3f9e1cf2c07a95ef131ddcfbae4d13e9e9c
Icaro Gabriel <100494049+Icaroiz@users.noreply.github.com>
2023-01-26 12:05:24 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "GREG","greg@gmail.com",new Date(), 4000.0,department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted ! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller UPDATE ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
